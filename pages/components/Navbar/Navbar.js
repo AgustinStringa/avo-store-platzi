4044927c45607da8212cae9dcb75847ea561fbd8
Agustín Stringa <stringaagustin1@gmail.com>
2023-01-16 23:15:06 -0300

    enlazando / prefetching en etiquetas Link

---
+++ pages/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+const Navbar = () => {
+    return (
+        <nav>
+            <Link href='/about' >About</Link>
+            <Link href='/' >Home</Link>
+        </nav>
+    )
+}
+
+export default Navbar
