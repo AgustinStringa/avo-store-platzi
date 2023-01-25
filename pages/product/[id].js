93236e0c7b6d3ca7c0603d3f2a241611b9ad9812
Agustín Stringa <stringaagustin1@gmail.com>
2023-01-25 11:05:49 -0300

    starting product by id page

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import Navbar from '../components/Navbar/Navbar';
 const ProductItem = () => {
+    const [productData, setProductData] = useState({});
     const params = useRouter();
+    const getProductById = async (id) => {
+        const res = await fetch(`/api/avo/${id}`);
+        const data = await res.json();
+        setProductData(data);
+    }
+
+    useEffect(() => {
+        if (params.query.id) {
+            getProductById(params.query.id);
+            return;
+        }
+    }, [params.query.id])
     return (
         <div>
             <Navbar />
             <h1>mostrando producto con id {params?.query?.id}</h1>
+            <h2>{productData.name}</h2>
+            <p>{productData.price}</p>
+            <img src={productData.image} />
+            <p>{productData.attributes?.description}</p>
         </div>
     )
 }
