37b25477447bd0559eff25d3ef54b1ff4e5765ae
Agustín Stringa <stringaagustin1@gmail.com>
2023-01-25 09:30:25 -0300

    basic api fetching

--- pages/components/Navbar/Navbar.js
+++ pages/components/Navbar/Navbar.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import Link from 'next/link'
 const Navbar = () => {
+    const fetching = async () => {
+        const res = await fetch('api/avo/')
+        const data = await res.json();
+        console.log(data);
+    }
+    fetching();
     return (
         <nav>
             <Link href='/about' >About</Link>
