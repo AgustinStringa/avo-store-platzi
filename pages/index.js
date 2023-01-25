4de1adac946aaff0c3256ee735b0acbe69e7a574
Agustín Stringa <stringaagustin1@gmail.com>
2023-01-25 10:32:40 -0300

    getting products / init .env

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,20 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
+import React, { useEffect, useState } from 'react'
+import Navbar from './components/Navbar/Navbar';
+
 export const Home = () => {
+    const [productList, setProductList] = useState([]);
+    const getProducts = async () => {
+        try {
+            const res = await fetch('api/avo/')
+            const { data } = await res.json();
+            setProductList(data);
+        } catch (error) {
+            setProductList([]);
+        }
+    }
+    useEffect(() => {
+        getProducts();
+    }, [])
     return (<>
         <Navbar />
         <div>Home</div>
