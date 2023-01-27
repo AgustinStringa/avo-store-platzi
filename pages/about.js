4e31acc26adf351203c115bacb31325f069c8ad0
Agustín Stringa <stringaagustin1@gmail.com>
2023-01-27 11:51:06 -0300

    init styles

--- pages/about.js
+++ pages/about.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 const about = () => {
     return (
-        <>
+        <div className='container'>
             <Navbar />
             <div>about</div>
             <h1>About us!!!</h1>
-        </>
+        </div>
     )
 }
 
