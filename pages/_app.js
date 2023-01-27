e77b90e88969025d63d1fcc35147f895f053eea6
Agustín Stringa <stringaagustin1@gmail.com>
2023-01-27 11:28:15 -0300

    adding path alias

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import Layout from "./Containers/Layout/Layout"
+import Layout from "@containers/Layout/Layout"
 export default function MyApp({ Component, pageProps }) {
     return <Layout>
         <Component {...pageProps} />
