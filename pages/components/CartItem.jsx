4d29685caa969b560924e9ebabdc67507082e322
Agustín Stringa <stringaagustin1@gmail.com>
2023-02-08 10:37:00 -0300

    adding spinner to add element to cart

--- pages/components/CartItem.jsx
+++ pages/components/CartItem.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import ProductItem from "@pages/product/[id]";
+
 const CartItem = ({ itemInfo, setCart, cart }) => {
   const data = itemInfo[0];
   // if (!data) return null;
