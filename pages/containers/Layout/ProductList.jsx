4a2e41aa6a8756a33320552e1f37ccd92bf4c512
Agustín Stringa <stringaagustin1@gmail.com>
2023-02-08 10:54:10 -0300

    adding loader page

--- pages/containers/Layout/ProductList.jsx
+++ pages/containers/Layout/ProductList.jsx
@@ -1,29 +1,36 @@
-import React from 'react'
-import ProductCard from '@components/ProductCard'
+import React from "react";
+import ProductCard from "@components/ProductCard";
+import LoaderPage from "@components/LoaderPage";
 const ProductList = ({ productList }) => {
-    return (
-        <>
-            <style jsx>{`
-                .product-list {
-                    display: flex;
-                    flex-direction: column;
-                    align-items: center;
-                    gap: 1rem;
-                    margin: 2.5rem 0;
-                }
-                @media (min-width: 768px) {
-                    .product-list {
-                        display: grid;
-                        grid-template-columns: 50% 50%;
-                        
-                    }
-                }
-            `}</style>
-            <div className='product-list'>
-                {productList && productList.map((el) => <ProductCard key={el.id} productData={el} />)}
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <style jsx>{`
+        .product-list {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          gap: 1rem;
+          margin: 2.5rem 0;
+        }
+        @media (min-width: 768px) {
+          .product-list {
+            display: grid;
+            grid-template-columns: 50% 50%;
+          }
+        }
+      `}</style>
 
-export default ProductList
+      {productList.length > 0 ? (
+        <div className="product-list">
+          {productList.map((el) => (
+            <ProductCard key={el.id} productData={el} />
+          ))}
+        </div>
+      ) : (
+        <LoaderPage />
+      )}
+    </>
+  );
+};
+
+export default ProductList;
